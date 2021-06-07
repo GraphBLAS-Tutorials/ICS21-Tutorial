0c83e9a9bc4bcf88f85d3b9a41bcfce6a8f62152
Michel Pelletier <pelletier.michel@gmail.com>
2021-06-07 10:35:26 -0700

    peg pygraphblas to 4.2.2 for now, rename local image tags to represent ics.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t graphblas/pygraphblas-siam21-tutorial .
+docker build -t graphblas/pygraphblas-ics21-tutorial .
--- notebook.sh
+++ notebook.sh
@@ -1,4 +1,4 @@
 docker run --rm --user root -e NB_UID=$(id -u) -e NB_GID=$(id -g) -p 8888:8888 \
 	   -w /home/jovyan/workspace \
 	   -v `pwd`/notebook-workspace:/home/jovyan/workspace  \
-	   -it graphblas/pygraphblas-siam21-tutorial:latest
+	   -it graphblas/pygraphblas-ics21-tutorial:latest
